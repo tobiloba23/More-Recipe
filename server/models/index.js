9f48f34e4e0d623ad6211efb78b8cbd4a28be479
tobiloba23 <tobiloba.ades@gmail.com>
2017-10-07 16:02:30 +0100

    Corrected config.js file for databaseconnection

--- server/models/index.js
+++ server/models/index.js
@@ -10,7 +10,7 @@ const db = {};
 let sequelize;
 
 if (configEnv.use_env_variable) {
-  sequelize = new Sequelize(process.env[configEnv.use_env_variable]);
+  sequelize = new Sequelize(process.env[configEnv.use_env_variable], configEnv);
 } else {
   sequelize =
 new Sequelize(configEnv.database, configEnv.username, configEnv.password, configEnv);

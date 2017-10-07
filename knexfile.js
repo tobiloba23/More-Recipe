9c59f826d0d8f0f62492c58b3734696fa2d21aa0
tobiloba23 <tobiloba.ades@gmail.com>
2017-10-07 20:02:48 +0100

    installed knex and initialized it, updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,11 +2,20 @@
 
 module.exports = {
 
+  development: {
+    username: 'TOBI',
+    password: null,
+    database: 'more_recipes_dev',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
+  },
+
   production: {
-    client: 'pg',
+    client: 'postgres',
     connection: process.env.DATABASE_URL.concat('?ssl=true'),
     migrations: {
-      directory: './server/migrations'
+      directory: './server/migrations/knex_migrations'
     },
     useNullAsDefault: true
   }

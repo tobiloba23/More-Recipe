f1262c19f6a5ec7a3163b5208831da02b4d1a6f2
tobiloba23 <tobiloba.ades@gmail.com>
2017-10-07 17:13:47 +0100

    Corrected config.js file for databaseconnection

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,11 @@ export default {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres'
+    use_env_variable: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: true
+    },
   }
 };

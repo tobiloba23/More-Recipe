138b3d73c2998cca65cc827a4ceaab5a959a2796
tobiloba23 <tobiloba.ades@gmail.com>
2017-10-07 15:05:40 +0100

    Corrected config.js file for databaseconnection

--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,25 @@
-import fotenv from 'dotenv';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 export default {
-    development: {
-        username: 'TOBI',
-        password: null,
-        database: 'more_recipes_dev',
-        host: '127.0.0.1',
-        port: 5432,
-        dialect: 'postgres'
-      },
-      test: {
-        username: 'TOBI',
-        password: null,
-        database: 'more_recipes_dev',
-        host: '127.0.0.1',
-        port: 5432,
-        dialect: 'postgres'
-      },
-      production: {
-        use_env_variable: 'DATABASE_URL'
-      }
+  development: {
+    username: 'TOBI',
+    password: null,
+    database: 'more_recipes_dev',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'TOBI',
+    password: null,
+    database: 'more_recipes_dev',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL'
+  }
 };

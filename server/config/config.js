1983be1074a94faab8b17e47cffc6a226d5803a3
tobiloba23 <tobiloba.ades@gmail.com>
2017-10-14 19:38:40 +0100

    Debug Travis Issues

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,8 @@ export default {
     username: 'postgres',
     password: process.env.DB_TEST_PASSWORD,
     database: 'more_recipes_test',
+    host: 'localhost',
+    port: 5432,
     dialect: 'postgres'
   },
   production: {

a602bd28f004eaaa1db3b7c8c76ab1644fd8ebc0
tobiloba23 <tobiloba.ades@gmail.com>
2017-10-15 20:37:03 +0100

    Debug Travis Issues

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,6 @@
 module.exports = {
   development: {
-    username: 'TOBI',
-    password: process.env.DB_DEV_PASSWORD,
-    database: 'more_recipes_dev',
-    host: '127.0.0.1',
-    port: 5432,
-    dialect: 'postgres'
+
   },
   test: {
     username: 'tobi',

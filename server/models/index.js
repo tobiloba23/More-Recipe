0605885ad65118a481fd874ac07bcc653b5a80a4
tobiloba23 <tobiloba.ades@gmail.com>
2017-10-16 01:11:10 +0100

    Debug Travis Issues

--- server/models/index.js
+++ server/models/index.js
@@ -4,19 +4,12 @@ import Sequelize from 'sequelize';
 import config from '../config/config';
 
 const basename = path.basename(__filename);
-const env = 'wewew';
+const env = process.env.NODE_ENV || 'development';
 const configEnv = config[env];
 const db = {};
 let sequelize;
 
 
-if (configEnv.use_env_variable) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
-} else {
-  console.log(configEnv.database, configEnv.username, configEnv.password);
-  sequelize =
-new Sequelize(configEnv.database, configEnv.username, configEnv.password, configEnv);
-}
 
 fs
   .readdirSync(__dirname)
